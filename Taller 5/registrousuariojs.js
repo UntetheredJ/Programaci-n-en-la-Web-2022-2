6c238ea4cc3820e7e47590593ced9d9bbfa71047
UntetheredJ <101756457+UntetheredJ@users.noreply.github.com>
2022-12-05 09:40:06 -0500

    Update registrousuariojs.js

--- Taller 5/registrousuariojs.js
+++ Taller 5/registrousuariojs.js
@@ -5,6 +5,11 @@ function printError(elementId, message){
     document.getElementById(elementId).innerHTML = message
 }
 
+form.addEventListener("submit", e=>{
+    let regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+    let fechan = new Date().getFullYear() - new Date(fecha.value).getFullYear()
+})
+
 function showEnfermedades(){
     var checkboxEnfermedades = document.getElementById("checkboxEnfermedades")
     
